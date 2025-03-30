10af023801e9a7cfd8393065181e63a473056754
Brandon Graham-Knight <bgk60@alumni.ubc.ca>
2025-03-30 23:13:56 +0000

    Trains CIFAR-10

--- train.sh
+++ train.sh
@@ -36,90 +36,27 @@ do
         "--lr-method=plateau" \
         "--no-pretrained"
 
-    python -m citl train CIFAR10UB mnasnet_small \
-        "--augmentation-policy-path=./policies/cifar10.yaml" \
-        "--noise-level=${level}" \
-        "--loss-function=cross_entropy" \
-        "--lr-method=plateau" \
-        "--selectively-backpropagate" \
-        "--no-pretrained"
 done
 
-# python -m citl standardtrain CityscapesFine efficientnet-b0 \
-#     "--augmentation-policy-path=./policies/cityscapes.yaml" \
-#     "--lr-method=plateau"
+# METHOD ALPHA SWEEP
+alphas=(0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0.10 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19)
+level=0.2
 
-# # NORMAL BACKPROP BASELINES
+for alpha in "${alphas[@]}"
+do
+    for level in "${levels[@]}"
+    do
 
-# python -m citl train CelebA resnet18 \
-#     "--no-selectively-backpropagate" \
-#     "--alpha=0.10" \
-#     "--augmentation-policy-path=./policies/celeba.yaml" \
-#     "--lr-method=plateau"
-
-# python -m citl train CIFAR10UB mnasnet_small \
-#     "--augmentation-policy-path=./policies/cifar10.yaml" \
-#     "--no-selectively-backpropagate" \
-#     "--alpha=0.10" \
-#     "--lr-method=plateau" \
-#     "--method=score"
-
-# python -m citl train DFire mnasnet_small \
-#     "--augmentation-policy-path=./policies/DFire.yaml" \
-#     "--no-selectively-backpropagate" \
-#     "--alpha=0.10" \
-#     "--lr-method=plateau" \
-#     "--method=score"
-
-# python -m citl train CityscapesFine efficientnet-b0 \
-#     "--augmentation-policy-path=./policies/cityscapes.yaml" \
-#     "--no-selectively-backpropagate" \
-#     "--alpha=0.10" \
-#     "--lr-method=plateau" \
-#     "--method=score"
-
-# # METHOD ALPHA SWEEP
-
-# numbers=(0.10 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19)
-# level=0.2
-
-# for alpha in "${numbers[@]}"
-# do
-
-    # python -m citl train CIFAR10UB mnasnet_small \
-    #     "--augmentation-policy-path=./policies/cifar10.yaml" \
-    #     "--selectively-backpropagate" \
-    #     "--alpha=${alpha}" \
-    #     "--noise-level=${level}" \
-    #     "--loss-function=cross_entropy" \
-    #     "--lr-method=plateau"
-
-    # python -m citl train CelebA resnet18 \
-    #     "--selectively-backpropagate" \
-    #     "--alpha=${alpha}" \
-    #     "--augmentation-policy-path=./policies/celeba.yaml" \
-    #     "--lr-method=plateau"
-
-    # python -m citl train DFire mnasnet_small \
-    #     "--augmentation-policy-path=./policies/DFire.yaml" \
-    #     "--selectively-backpropagate" \
-    #     "--alpha=${alpha}" \
-    #     "--lr-method=plateau" \
-    #     "--method=score"
-
-    # python -m citl train CIFAR10 mnasnet_small  \
-    #     "--augmentation-policy-path=./policies/cifar10.yaml" \
-    #     "--selectively-backpropagate" \
-    #     "--alpha=${alpha}" \
-    #     "--lr-method=plateau" \
-    #     "--method=score"
-
-    # python -m citl train CityscapesFine efficientnet-b0 \
-    #     "--augmentation-policy-path=./policies/cityscapes.yaml" \
-    #     "--selectively-backpropagate" \
-    #     "--alpha=${alpha}" \
-    #     "--lr-method=plateau" \
-    #     "--method=score"
-# done
+        python -m citl train CIFAR10UB mnasnet_small \
+            "--augmentation-policy-path=./policies/cifar10.yaml" \
+            "--noise-level=${level}" \
+            "--alpha=${alpha}" \
+            "--loss-function=cross_entropy" \
+            "--lr-method=plateau" \
+            "--selectively-backpropagate" \
+            "--no-pretrained"
+    
+    done
+done
 
 
