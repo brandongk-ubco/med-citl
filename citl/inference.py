4e5031a1eaa15982897c15b5b7cbcb1af0fc2303
Brandon Graham-Knight <bgk60@alumni.ubc.ca>
2025-04-06 21:00:56 +0000

    Inference optimizations.

--- citl/inference.py
+++ citl/inference.py
@@ -2,18 +2,19 @@ import neptune
 from . import cli
 from loguru import logger
 from .dataset import Dataset
-import pandas as pd
 import onnxruntime
 from tqdm import tqdm
 import numpy as np
 from torchmetrics.classification import JaccardIndex
 import torch
 
+
 def softmax(x):
-  """Compute softmax values along the first axis (index 0) of a 3D array."""
-  # Keep the max along axis 0 for numerical stability
-  e_x = np.exp(x - np.max(x, axis=0, keepdims=True))
-  return e_x / np.sum(e_x, axis=0, keepdims=True)
+    """Compute softmax values along the first axis (index 0) of a 3D array."""
+    # Keep the max along axis 0 for numerical stability
+    e_x = np.exp(x - np.max(x, axis=0, keepdims=True))
+    return e_x / np.sum(e_x, axis=0, keepdims=True)
+
 
 @cli.command()
 def inference(run_id: str, dataset: Dataset):
@@ -47,6 +48,15 @@ def inference(run_id: str, dataset: Dataset):
     predictions = []
     ground_truths = []
 
+    dataloader = datamodule.test_dataloader()
+
+    predictions = np.empty(
+        (len(dataloader), datamodule.num_classes, 1024, 2048), dtype=np.float16
+    )
+    ground_truths = np.empty((len(dataloader), 1024, 2048), dtype=np.uint8)
+
+    i = 0
+
     for batch in tqdm(datamodule.test_dataloader(), desc="Inferencing"):
         inputs, targets, _index = batch
 
@@ -57,13 +67,23 @@ def inference(run_id: str, dataset: Dataset):
             target = targets[idx]
 
             input_np = input.numpy()
-            prediction_0 = ort_session.run([output_name], {input_name: input_np[:,:512,:1024]})
-            prediction_1 = ort_session.run([output_name], {input_name: input_np[:,:512,1024:]})
-            prediction_2 = ort_session.run([output_name], {input_name: input_np[:,512:,:1024]})
-            prediction_3 = ort_session.run([output_name], {input_name: input_np[:,512:,1024:]})
-            
+            prediction_0 = ort_session.run(
+                [output_name], {input_name: input_np[:, :512, :1024]}
+            )
+            prediction_1 = ort_session.run(
+                [output_name], {input_name: input_np[:, :512, 1024:]}
+            )
+            prediction_2 = ort_session.run(
+                [output_name], {input_name: input_np[:, 512:, :1024]}
+            )
+            prediction_3 = ort_session.run(
+                [output_name], {input_name: input_np[:, 512:, 1024:]}
+            )
 
-            stitched_prediction = np.zeros((datamodule.num_classes, input_np.shape[1], input_np.shape[2]), dtype=prediction_0[0].dtype)
+            stitched_prediction = np.zeros(
+                (datamodule.num_classes, input_np.shape[1], input_np.shape[2]),
+                dtype=prediction_0[0].dtype,
+            )
 
             stitched_prediction[:, :512, :1024] = prediction_0[0]
             stitched_prediction[:, :512, 1024:] = prediction_1[0]
@@ -71,16 +91,22 @@ def inference(run_id: str, dataset: Dataset):
             stitched_prediction[:, 512:, 1024:] = prediction_3[0]
 
             prediction = softmax(stitched_prediction)
-            jaccard(torch.Tensor(prediction).unsqueeze(0), torch.Tensor(target).unsqueeze(0))
-            predictions.append(prediction.astype(np.float16))
-            ground_truths.append(target.numpy())
+            jaccard(
+                torch.Tensor(prediction).unsqueeze(0), torch.Tensor(target).unsqueeze(0)
+            )
+            predictions[i, :, :, :] = stitched_prediction.astype(np.float16)
+            ground_truths[i, :, :] = target.numpy()
 
+            i += 1
 
-    predictions = np.stack(predictions)
-    ground_truths = np.stack(ground_truths)
+    np.savez_compressed(
+        f"{run_id}.npz",
+        predictions=np.stack(predictions),
+        ground_truths=np.stack(ground_truths),
+    )
 
-    np.savez_compressed(f"{run_id}.npz", predictions=predictions, ground_truths=ground_truths)
-
-    jaccards = dict(zip(datamodule.classes, [n * 100 for n in jaccard.compute().tolist()]))
+    jaccards = dict(
+        zip(datamodule.classes, [n * 100 for n in jaccard.compute().tolist()])
+    )
     logger.info(f"IoU: {jaccards}")
     logger.info(f"Mean IoU: {jaccard.compute()[1:].mean() * 100:.2f}")
